7ca3ad5209b854826c379c0dc8e7d5a8560a10bf
BobAnkh <bobankhshen@gmail.com>
2021-05-23 09:01:20 +0000

    feat(parse): add area plot and support iteration

--- log_parse.py
+++ log_parse.py
@@ -5,6 +5,7 @@ import toml
 
 from utils.ccp_parse import bbr_parse
 from utils.parseTputDelay import parse_tput_delay, plot_tput_delay
+from utils.area import plot_area
 
 configs = toml.load('config.toml')
 
@@ -18,13 +19,23 @@ ccp_fig_folder = configs['path']['ccp_fig_folder']
 mahimahi_fig_folder = configs['path']['mahimahi_fig_folder']
 binsize = configs['data']['log']['binsize']
 duration = configs['data']['log']['duration']
+iteration = configs['data']['iteration']
+area_fig_folder = configs['path']['area_fig_folder']
 
 ccp_algs = configs['data']['ccp_algs']
 
 trace_info = json.load(
     open(os.path.join(trace_folder, 'trace_info.json'), encoding='utf-8'))
 
-bbr_parse(trace_info, delay_list, log_folder,
+print('parse bbr ccp log')
+bbr_parse(trace_info, delay_list, iteration, log_folder,
           os.path.join(fig_folder, ccp_fig_folder))
 
-plot_tput_delay(ccp_algs, packet_buffer_list, trace_info, delay_list, log_folder, os.path.join(fig_folder, mahimahi_fig_folder), duration, binsize)
+print('parse mahimahi log')
+mahimahi_results = parse_tput_delay(log_folder, duration, binsize)
+
+print('plot tput and delay')
+plot_tput_delay(ccp_algs, packet_buffer_list, trace_info, delay_list, iteration, mahimahi_results, os.path.join(fig_folder, mahimahi_fig_folder))
+
+print('parse mahimahi log and ')
+area_dict = plot_area(ccp_algs, packet_buffer_list, trace_info, delay_list, iteration, mahimahi_results, os.path.join(fig_folder, area_fig_folder))
