dcc4ec11ee9aba36cafd8c8fb4ecf001620b72ed
BobAnkh <bobankhshen@gmail.com>
2021-06-23 10:00:51 +0000

    feat(area): add comments and todos

--- utils/area.py
+++ utils/area.py
@@ -11,51 +11,82 @@ from tqdm import tqdm
 from utils import parseTputDelay, tools
 
 
-def calculate_area(time_list, tput_list, start_time, end_time, standard_bw):
-    area_sum = 0
-    count = 0
-    for i, t in enumerate(time_list):
-        if t < start_time:
-            continue
-        if t >= end_time:
-            break
-        area_sum = area_sum + abs(standard_bw - tput_list[i])
-        count = count + 1
-    return area_sum / count * (end_time - start_time)
-
-
-def calculate_area_detail(time_list, tput_list, start_time, end_time,
+def calculate_area_detail(time_list, tput_list, start_time_seq, end_time_seq,
                           standard_bw):
+    '''
+    Calculate area between throughput and standard bandwidth from start time to end time.
+
+    Args:
+        time_list (list): the whole time sequence
+        tput_list (list): the whole throughput sequence corresponding to the time_list
+        start_time_seq (int or float): start time sequence number
+        end_time_seq (int or float): end time sequence number
+        standard_bw (int or float): standard bandwidth
+
+    Returns:
+        float: area normalized by reaction time
+    '''
     area_sum = 0
     for i, t in enumerate(time_list):
-        if i < start_time:
+        if i < start_time_seq:
             continue
-        if i >= end_time:
+        if i >= end_time_seq:
             break
         area_sum = area_sum + abs(standard_bw -
                                   tput_list[i]) * (time_list[i + 1] - t)
-    return area_sum / (time_list[end_time] - time_list[start_time])
+    # TODO: may not normalize area by reaction time
+    return area_sum / (time_list[end_time_seq] - time_list[start_time_seq])
 
 
-def wnd_detect(time_list, start_time, start_seq, end_time, wnd_size):
-    wnd_end = -1
-    end_seq = -1
+def wnd_detect(time_list, start_wnd_time, start_wnd_seq, end_time, wnd_size):
+    '''
+    Search a best match point of the window end from the time sequence.
+
+    Args:
+        time_list (list): the whole time sequence
+        start_wnd_time (int or float): window start time
+        start_wnd_seq (int): window start time sequence number in time_list
+        end_time (int or float): end time
+        wnd_size (int or float): window size in milleseconds
+
+    Returns:
+        float, int, int: end time of the window, end time sequence number of the window, error code
+    '''
+    end_wnd_time = -1
+    end_wnd_seq = -1
     error = -1
-    if time_list[start_seq] != start_time:
-        return wnd_end, end_seq, error
-    for ii, tt in enumerate(time_list[start_seq:]):
+    if time_list[start_wnd_seq] != start_wnd_time:
+        return end_wnd_time, end_wnd_seq, error - 2
+    for ii, tt in enumerate(time_list[start_wnd_seq:]):
         if tt >= end_time:
-            return wnd_end, end_seq, error
-        if tt >= start_time + wnd_size:
-            wnd_end = tt
-            end_seq = ii + start_seq
+            return end_wnd_time, end_wnd_seq, error - 1
+        if tt >= start_wnd_time + wnd_size:
+            end_wnd_time = tt
+            end_wnd_seq = ii + start_wnd_seq
             error = 0
-            return wnd_end, end_seq, error
-    return wnd_end, end_seq, error
+            return end_wnd_time, end_wnd_seq, error
+    return end_wnd_time, end_wnd_seq, error
 
 
 def stable_detect(time_list, tput_list, start_time, end_time, wnd_size,
                   move_size, wnd_num, mean_oscillation, var_oscillation):
+    '''
+    Detect when the CCA enters steady state.
+
+    Args:
+        time_list (list): the whole time sequence
+        tput_list (list): the whole throughput sequence corresponding to the time_list
+        start_time (int or float): start time
+        end_time (int or float): end time
+        wnd_size (int or float): window size in milleseconds
+        move_size (int or float): time interval in milleseconds between adjacent two windows' start time
+        wnd_num (int): how much windows are used in stable detection
+        mean_oscillation (float): oscillation extent of means of windows
+        var_oscillation (float): oscillation extent of vars of windows
+
+    Returns:
+        int: sequence number in time list of the point entering steady state
+    '''
     for i, t in enumerate(time_list):
         if t < start_time:
             continue
@@ -75,6 +106,8 @@ def stable_detect(time_list, tput_list, start_time, end_time, wnd_size,
             wnd_var = np.var(tput_list[start_seq:end_seq + 1])
             wnd_list.append(np.array([wnd_mean, wnd_var]))
             if num == int(wnd_num / 2):
+                # here we use the middle of windows, but we may can use the front of the windows
+                # TODO: use the front of the windows as the stable point
                 stable_mark = int((start_seq + end_seq) / 2)
         wnd_list = np.array(wnd_list)
         # print(np.var(wnd_list[:, 0]) , np.var(wnd_list[:, 1]))
@@ -84,7 +117,23 @@ def stable_detect(time_list, tput_list, start_time, end_time, wnd_size,
 
 
 def plot_area(ccp_algs, packet_buffer_list, trace_info, delay_list, iteration,
-              mahimahi_results, fig_folder):
+              mahimahi_results, fig_folder, name_map):
+    '''
+    Calculate area and plot figures.
+
+    Args:
+        ccp_algs (dict): CCP algorithms dict
+        packet_buffer_list (list): packet buffer list
+        trace_info (dict): traces information
+        delay_list (list): delay list
+        iteration (int): number of iterations
+        mahimahi_results (dict): parsed mahimahi log
+        fig_folder (str): folder to save figures
+        name_map (dict): map ccp algorithm's name to actual CCA's name
+
+    Returns:
+        dict: area info
+    '''
     tools.clear_folder(fig_folder)
     print('Parse Area...')
     area_dict = {}
@@ -100,16 +149,18 @@ def plot_area(ccp_algs, packet_buffer_list, trace_info, delay_list, iteration,
                     for iter_num in range(iteration):
                         # print('iter_num', iter_num)
                         for ccp_alg in ccp_algs:
-                            log_name = f'{ccp_alg}-{link_trace}-{delay}-{delay_var}-{iter_num}-mahimahi.log'
+                            log_name = f'{ccp_alg}-{link_trace}-{packet_buffer}-{delay}-{delay_var}-{iter_num}-mahimahi.log'
+                            # TODO: may update stable detect params config
                             stable_mark = stable_detect(
                                 mahimahi_results[log_name]['time_list'],
-                                mahimahi_results[log_name]['tput_list'], 0, 5,
-                                delay * 10 / 1000, delay * 4 / 1000, 3, 0.06,
-                                0.06)
+                                mahimahi_results[log_name]['tput_list'], 0, 40,
+                                delay * 20 / 1000, delay * 8 / 1000, 3, 0.02,
+                                0.02)
+                            # TODO: normalize by bandwidth
                             area = calculate_area_detail(
                                 mahimahi_results[log_name]['time_list'],
                                 mahimahi_results[log_name]['tput_list'], 0,
-                                stable_mark, standard_bw)
+                                stable_mark, standard_bw) /(standard_bw/12)
                             # print(log_name, stable_mark)
                             if ccp_alg not in alg_dict:
                                 alg_dict[ccp_alg] = [area]
@@ -122,14 +173,11 @@ def plot_area(ccp_algs, packet_buffer_list, trace_info, delay_list, iteration,
                                 stable_dict[ccp_alg].append(
                                     mahimahi_results[log_name]['time_list']
                                     [stable_mark])
-                            # print(f'{ccp_alg}: {area}', end=' ')
-                        # print(' ')
                     area_dict[
                         f'{link_trace}-{packet_buffer}-{delay}-{delay_var}'] = {
                             'alg_dict': alg_dict,
                             'stable_dict': stable_dict
                         }
-                    # print('\n')
     json.dump(
         area_dict,
         open(os.path.join(fig_folder, 'area_info.json'), 'w',
@@ -148,12 +196,19 @@ def plot_area(ccp_algs, packet_buffer_list, trace_info, delay_list, iteration,
         plt.savefig(os.path.join(fig_folder, f'{scenario}-area.png'))
         plt.close()
     for scenario in area_dict:
-        plt.figure()
+        plt.figure(figsize=(6.4, 3))
         df = pd.DataFrame.from_dict(area_dict[scenario]['alg_dict'],
                                     orient='index').T
         df = df.drop(index=list(df.idxmax(axis=0)))
-        sns.violinplot(data=df, palette='Set2')
-        plt.title(scenario)
-        plt.savefig(os.path.join(fig_folder, f'{scenario}-violin.png'))
+        df.columns = [name_map[x] for x in df.columns]
+
+        b = sns.violinplot(data=df, palette='Set2')
+        print(plt.yticks())
+        _, xlabels = plt.xticks()
+        b.set_yticklabels(b.get_yticks(), size = 20)
+        b.set_xticklabels(xlabels, size = 20)
+        # plt.title(scenario)
+        plt.ylabel('Area', fontsize='20')
+        plt.savefig(os.path.join(fig_folder, f'{scenario}-violin.pdf'), bbox_inches='tight')
         plt.close()
     return area_dict
